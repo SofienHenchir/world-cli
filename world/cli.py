99cded4fa376e10032ba3b8d87a39742f216787a
henchir <sofien.henchir.tn@gmail.com>
2026-07-24 17:29:43 +0200

    refactor solution

---
+++ world/cli.py
@@ -0,0 +1,21 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+# from dotenv import load_dotenv
+
+
+def main():
+    print("Welcome to World CLI!")
+    load_dotenv()
+
+    user_name = input("Enter your name: ")
+    country_search = input("Enter a country name: ")
+    url = os.getenv("BASE_URL") + country_search
+    headers = {"X-Api-Key": os.getenv("API_Key")}
+    response = requests.get(url, headers=headers)
+    print(
+        f"{user_name} is looking for informations aboutt {country_search}.. Fetching data .."
+    )
+    print(response.json())
